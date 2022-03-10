ee0b65d57979e9c9c5b978e14f13415c139708dc
DamirNabiull <51910382+DamirNabiull@users.noreply.github.com>
2022-03-10 17:48:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import json
+
+file = open('config.json')
+data = json.load(file)
+
+chrome_options = Options()
+chrome_service = Service(data['service'])
+chrome_options.add_experimental_option("useAutomationExtension", False)
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_argument("--kiosk");
+
+driver = webdriver.Chrome(service=chrome_service,
+                          options=chrome_options)
+driver.get(data['url'])
