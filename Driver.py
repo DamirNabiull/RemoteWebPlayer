769ccd8d6a1cb3dba5bd308c72600b3d8e2e226a
DamirNabiull <51910382+DamirNabiull@users.noreply.github.com>
2022-03-29 00:07:15 +0300

    Create Driver.py

---
+++ Driver.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import json
+import logging
+import sys
+import os
+
+
+logging.basicConfig(filename='app_player.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
+
+class MyDriver:
+    __instance = None
+
+    def __init__(self):
+        if not MyDriver.__instance:
+            file = open('config.json')
+            self.data = json.load(file)
+
+            chrome_options = Options()
+            chrome_service = Service(self.data[self.data['platform']])
+            chrome_options.add_experimental_option("useAutomationExtension", False)
+            chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+            chrome_options.add_argument("--kiosk")
+
+            self.driver = webdriver.Chrome(service=chrome_service,
+                                      options=chrome_options)
+
+            self.driver.set_page_load_timeout(5)
+            if self.data['show'] == 'url':
+                try:
+                    self.driver.get(self.GetLink())
+                except Exception as e:
+                    page_state = self.driver.execute_script('return document.readyState;') == 'complete'
+                    logging.warning(f'Start : {page_state}')
+            else:
+                try:
+                    path = self.data['projectPath']
+                    self.driver.get(rf'{path}\image.png')
+                except Exception as e:
+                    page_state = self.driver.execute_script('return document.readyState;') == 'complete'
+                    logging.warning(f'Start : {page_state}')
+
+
+    @classmethod
+    def getInstance(cls):
+        if not cls.__instance:
+            cls.__instance = MyDriver()
+        return cls.__instance
+
+    def GetDriver(self):
+        return self.driver
+
+    def GetLink(self):
+        return self.data['url']
