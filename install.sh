6237826e4c081081828a67257426d88015f90530
DamirNabiull <51910382+DamirNabiull@users.noreply.github.com>
2022-03-29 20:34:46 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+sudo apt-get update
+
+sudo apt-get install chromium-driver
+
+sudo apt-get install openssh-server
+sudo uwf allow ssh
+
+sudo apt-get install python3.9
+sudo apt-get install python3-pip
 sudo apt-get install python-selenium python3-selenium
 sudo pip3 install -r requirements.txt
-wget https://chromedriver.storage.googleapis.com/99.0.4844.51/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-./chromedriver
