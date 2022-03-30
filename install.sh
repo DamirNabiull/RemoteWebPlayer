58ed0c5a8136aec338e126cffa1b5da19de9c447
Damir <nabiull2020@gmail.com>
2022-03-30 11:57:21 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install chromium-driver
+sudo apt-get install chromium-driver -y
 
-sudo apt-get install openssh-server
-sudo uwf allow ssh
+sudo apt-get install openssh-server -y
+sudo uwf allow ssh -y
 
-sudo apt-get install python3.9
-sudo apt-get install python3-pip
-sudo apt-get install python-selenium python3-selenium
-sudo pip3 install -r requirements.txt
+sudo apt-get install python3.9 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python-selenium python3-selenium -y
+sudo pip3 install -r requirements.txt -y
