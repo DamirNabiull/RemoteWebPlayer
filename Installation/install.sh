79acbf9141ff40d52dc74fa1e2991ef418ee3488
Damir <nabiull2020@gmail.com>
2022-03-31 16:33:22 +0300

    Updated install.sh

--- Installation/install.sh
+++ Installation/install.sh
@@ -3,10 +3,14 @@ sudo apt-get update -y
 
 sudo apt-get install chromium-driver -y
 
+sudo apt-get install vlc -y
+
 sudo apt-get install openssh-server -y
 sudo uwf allow ssh -y
 
 sudo apt-get install python3.9 -y
 sudo apt-get install python3-pip -y
 sudo apt-get install python-selenium python3-selenium -y
+
 sudo pip3 install -r requirements.txt -y
+sudo pip3 install --upgrade requests -y
