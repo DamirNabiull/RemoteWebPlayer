10b2a0e7a2e6c4a70da5a2c1f0c8c6466717ebef
Damir <nabiull2020@gmail.com>
2022-03-31 16:33:00 +0300

    Deleted unnecessary lines

--- Application/main.py
+++ Application/main.py
@@ -1,6 +1,5 @@
 from aiohttp import web
 from routes import routes
-from Driver import MyDriver
 import json
 import os
 
@@ -8,7 +7,7 @@ import os
 if __name__ == '__main__':
     file = open('config.json')
     config = json.load(file)
-    config['projectPath'] = os.getcwd()[:-12]
+    config['projectPath'] = os.getcwd()
     with open('config.json', 'w') as outfile:
         json.dump(config, outfile)
 
@@ -16,5 +15,3 @@ if __name__ == '__main__':
     app.add_routes(routes)
     web.run_app(app, host='0.0.0.0', port=8080)
 
-    myDriver = MyDriver.getInstance()
-    driver = myDriver.GetDriver()
